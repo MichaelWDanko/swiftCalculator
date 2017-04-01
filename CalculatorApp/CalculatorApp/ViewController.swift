f96521d960832cc49e23096e9611f5696bf39952
Michael Danko <MichaelDanko@icloud.com>
2017-03-31 21:06:47 -0400

    Setup initial UI and the outline of initialized labels, text fields, & buttons.

--- CalculatorApp/CalculatorApp/ViewController.swift
+++ CalculatorApp/CalculatorApp/ViewController.swift
@@ -10,15 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblHeader: UILabel!
+    @IBOutlet weak var txtFirstNumber: UITextField!
+    @IBOutlet weak var txtSecondNumber: UITextField!
+    @IBOutlet weak var txtResults: UITextField!
+    
+    @IBOutlet weak var btnAddOUTLET: UIButton!
+    @IBOutlet weak var btnSubtractOUTLET: UIButton!
+    @IBOutlet weak var btnMultiplyOUTLET: UIButton!
+    @IBOutlet weak var btnDivideOUTLET: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        initText()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func initText(){
+        lblHeader.text?.uppercased()
+        
+    }
+
+    @IBAction func btnAddACTION(_ sender: UIButton) {
+        
+    }
+    @IBAction func btnSubtractACTION(_ sender: UIButton) {
+        
+    }
+    @IBAction func btnMultiplyACTION(_ sender: UIButton) {
+        
+    }
+    @IBAction func btnDivideACTION(_ sender: UIButton) {
+        
+    }
+   
 
 
 }
